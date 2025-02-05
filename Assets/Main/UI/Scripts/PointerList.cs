bfecca1247a4c30bf201b6bc9d3d2fdd66d8bcf0
Bukasker <kamil.buk.01@gmail.com>
2025-02-05 11:10:44 +0100

    Changed pointers list name

--- Assets/Main/UI/Scripts/PointerList.cs
+++ Assets/Main/UI/Scripts/PointerList.cs
@@ -1,8 +1,40 @@
+using Mapbox.Utils;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PointerList : MonoBehaviour
 {
-    [SerializeField] public List<GameObject> pointers;
+    public List<GameObject> Pointers;
+    public List<LineRenderer> Lines;
+    public List<GameObject> Arrows;
+    public Vector2d[] Locations;
+
+    public static PointerList Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        Pointers.RemoveAll(obj => obj == null);
+        Lines.RemoveAll(obj => obj == null);
+        Arrows.RemoveAll(obj => obj == null);
+    }
 }
