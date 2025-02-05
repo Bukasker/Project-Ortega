e39375881f2a21a205ef76676a4e6ed255772537
Bukasker <kamil.buk.01@gmail.com>
2025-02-05 16:15:03 +0100

    Updated save system for android

--- Assets/Main/Code/Scripts/SaveLoad/SaveSystem.cs
+++ Assets/Main/Code/Scripts/SaveLoad/SaveSystem.cs
@@ -2,7 +2,6 @@ using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.Serialization.Formatters.Binary;
-using UnityEngine.SceneManagement;
 using System;
 
 public class SaveSystem : MonoBehaviour
@@ -10,16 +9,21 @@ public class SaveSystem : MonoBehaviour
     [SerializeField] EventPointer ponterPrefab;
     [SerializeField] GameObject linePrefab;
     [SerializeField] GameObject arrowPrefab;
+
     public static List<EventPointer> mapPoints = new List<EventPointer>();
     public static List<AddLineToSaveSystem> lineGameObjects = new List<AddLineToSaveSystem>();
     public static List<AddArrowToSaveSystem> arrowGameObjects = new List<AddArrowToSaveSystem>();
 
-    const string SaveFolder = "/Saves/";
+    private string saveFolder;
 
     private void Awake()
     {
-        TestLoadLatestSave();
-        Directory.CreateDirectory(Application.persistentDataPath + SaveFolder); // Upewnij siê, ¿e folder istnieje
+        saveFolder = Application.persistentDataPath + "/Saves/";
+
+        if (!Directory.Exists(saveFolder))
+        {
+            Directory.CreateDirectory(saveFolder);  // Tworzy katalog, jeœli nie istnieje
+        }
     }
 
     private void OnApplicationQuit()
@@ -30,127 +34,107 @@ public class SaveSystem : MonoBehaviour
     public void SaveGame()
     {
         string path = GenerateSavePath();
-        BinaryFormatter formatter = new BinaryFormatter();
-        FileStream stream = new FileStream(path, FileMode.Create);
 
-        // Zapisz dane obiektów
-        formatter.Serialize(stream, mapPoints.Count);
-        foreach (var point in mapPoints)
+        try
         {
-            MapPointData data = new MapPointData(point);
-            formatter.Serialize(stream, data);
-        }
-
-        formatter.Serialize(stream, lineGameObjects.Count);
-        foreach (var line in lineGameObjects)
-        {
-            LineData data = new LineData(line);
-            formatter.Serialize(stream, data);
-        }
-
-        formatter.Serialize(stream, arrowGameObjects.Count);
-        foreach (var arrow in arrowGameObjects)
-        {
-            ArrowData data = new ArrowData(arrow);
-            formatter.Serialize(stream, data);
-        }
-
-        stream.Close();
-        Debug.Log("Gra zapisana w " + path);
-    }
-    public void TestLoadLatestSave()
-    {
-        string latestSavePath = GetLatestSavePath();
-        if (!string.IsNullOrEmpty(latestSavePath) && File.Exists(latestSavePath))
-        {
-            Debug.Log("Próba wczytania ostatniego zapisu z pliku: " + latestSavePath);
-            LoadGame(); // Wczytaj ostatni zapis
-            Debug.Log("Ostatni zapis wczytany pomyœlnie.");
-        }
-        else
-        {
-            Debug.LogWarning("Brak zapisów do wczytania.");
-        }
-    }
-    public void LoadGame(string saveFileName = null)
-    {
-        string path = string.IsNullOrEmpty(saveFileName)
-            ? GetLatestSavePath()
-            : Application.persistentDataPath + SaveFolder + saveFileName;
-
-        if (File.Exists(path))
-        {
-            BinaryFormatter formatter = new BinaryFormatter();
-            FileStream stream = new FileStream(path, FileMode.Open);
-
-            // Wczytaj dane obiektów
-            int mapPointCount = (int)formatter.Deserialize(stream);
-            for (int i = 0; i < mapPointCount; i++)
+            using (FileStream stream = new FileStream(path, FileMode.Create))
             {
-                MapPointData data = formatter.Deserialize(stream) as MapPointData;
-                Vector3 position = new Vector3(data.position[0], data.position[1], data.position[2]);
-                EventPointer ponter = Instantiate(ponterPrefab, position, Quaternion.identity);
-                ponter.eventPos[0] = Convert.ToDouble(data.locationStrings[0]);
-                ponter.eventPos[1] = Convert.ToDouble(data.locationStrings[1]);
-            }
+                BinaryFormatter formatter = new BinaryFormatter();
 
-            int lineObjectCount = (int)formatter.Deserialize(stream);
-            for (int i = 0; i < lineObjectCount; i++)
-            {
-                LineData data = formatter.Deserialize(stream) as LineData;
-                Vector3 position = new Vector3(data.position[0], data.position[1], data.position[2]);
-                Quaternion rotation = Quaternion.Euler(data.rotation[0], data.rotation[1], data.rotation[2]);
-
-                // Tworzymy instancjê linii
-                var lineInstance = Instantiate(linePrefab, position, rotation);
-
-                // Pobieramy komponent LineRenderer i ustawiamy jego pozycje
-                var lineRenderer = lineInstance.GetComponent<LineRenderer>();
-                if (lineRenderer != null)
+                formatter.Serialize(stream, mapPoints.Count);
+                foreach (var point in mapPoints)
                 {
-                    Vector3 lineStart = new Vector3(data.lineStart[0], data.lineStart[1], data.lineStart[2]);
-                    Vector3 lineEnd = new Vector3(data.lineEnd[0], data.lineEnd[1], data.lineEnd[2]);
+                    formatter.Serialize(stream, new MapPointData(point));
+                }
 
-                    lineRenderer.SetPosition(0, lineStart);
-                    lineRenderer.SetPosition(1, lineEnd);
+                formatter.Serialize(stream, lineGameObjects.Count);
+                foreach (var line in lineGameObjects)
+                {
+                    formatter.Serialize(stream, new LineData(line));
+                }
 
-                    lineRenderer.startColor = Color.red;
-                    lineRenderer.endColor = Color.red;
-
-                    if (lineRenderer.material == null)
-                    {
-                        Material lineMaterial = new Material(Shader.Find("Sprites/Default")); // Mo¿esz wybraæ inny shader
-                        lineRenderer.material = lineMaterial;
-                    }
+                formatter.Serialize(stream, arrowGameObjects.Count);
+                foreach (var arrow in arrowGameObjects)
+                {
+                    formatter.Serialize(stream, new ArrowData(arrow));
                 }
             }
 
-            int arrowObjectCount = (int)formatter.Deserialize(stream);
-            for (int i = 0; i < arrowObjectCount; i++)
-            {
-                ArrowData data = formatter.Deserialize(stream) as ArrowData;
-                Vector3 position = new Vector3(data.position[0], data.position[1], data.position[2]);
-                Quaternion rotation = Quaternion.Euler(data.rotation[0], data.rotation[1], data.rotation[2]);
-                Instantiate(arrowPrefab, position, rotation);
-            }
-
-            stream.Close();
-            Debug.Log("Gra wczytana z " + path);
+            Debug.Log("Gra zapisana w " + path);
         }
-        else
+        catch (Exception e)
         {
-            Debug.LogError("Plik zapisu nie znaleziony w " + path);
+            Debug.LogError("B³¹d podczas zapisywania gry: " + e.Message);
         }
     }
 
+    public void LoadGame(string saveFileName = null)
+    {
+        string path = string.IsNullOrEmpty(saveFileName) ? GetLatestSavePath() : saveFolder + saveFileName;
+
+        if (!File.Exists(path))
+        {
+            Debug.LogError("Plik zapisu nie znaleziony w " + path);
+            return;
+        }
+
+        try
+        {
+            using (FileStream stream = new FileStream(path, FileMode.Open))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+
+                int mapPointCount = (int)formatter.Deserialize(stream);
+                for (int i = 0; i < mapPointCount; i++)
+                {
+                    MapPointData data = formatter.Deserialize(stream) as MapPointData;
+                    Vector3 position = new Vector3(data.position[0], data.position[1], data.position[2]);
+                    Instantiate(ponterPrefab, position, Quaternion.identity);
+                }
+
+                int lineObjectCount = (int)formatter.Deserialize(stream);
+                for (int i = 0; i < lineObjectCount; i++)
+                {
+                    LineData data = formatter.Deserialize(stream) as LineData;
+                    Vector3 position = new Vector3(data.position[0], data.position[1], data.position[2]);
+                    Quaternion rotation = Quaternion.Euler(data.rotation[0], data.rotation[1], data.rotation[2]);
+                    Instantiate(linePrefab, position, rotation);
+                }
+
+                int arrowObjectCount = (int)formatter.Deserialize(stream);
+                for (int i = 0; i < arrowObjectCount; i++)
+                {
+                    ArrowData data = formatter.Deserialize(stream) as ArrowData;
+                    Vector3 position = new Vector3(data.position[0], data.position[1], data.position[2]);
+                    Quaternion rotation = Quaternion.Euler(data.rotation[0], data.rotation[1], data.rotation[2]);
+                    Instantiate(arrowPrefab, position, rotation);
+                }
+            }
+
+            Debug.Log("Gra wczytana z " + path);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("B³¹d podczas wczytywania gry: " + e.Message);
+        }
+    }
     public List<string> GetSaveList()
     {
-        string[] files = Directory.GetFiles(Application.persistentDataPath + SaveFolder, "*.sav");
         List<string> saveFiles = new List<string>();
-        foreach (var file in files)
+
+        if (Directory.Exists(saveFolder))
         {
-            saveFiles.Add(Path.GetFileName(file));
+            string[] files = Directory.GetFiles(saveFolder, "*.sav");
+            foreach (var file in files)
+            {
+                saveFiles.Add(Path.GetFileName(file));
+            }
         }
+        else
+        {
+            Debug.LogWarning("Folder zapisu nie istnieje: " + saveFolder);
+        }
+
         return saveFiles;
     }
 
@@ -160,7 +144,7 @@ public class SaveSystem : MonoBehaviour
         string path;
         do
         {
-            path = Application.persistentDataPath + SaveFolder + "save_" + saveIndex + ".sav";
+            path = saveFolder + "save_" + saveIndex + ".sav";
             saveIndex++;
         } while (File.Exists(path));
 
@@ -169,7 +153,7 @@ public class SaveSystem : MonoBehaviour
 
     private string GetLatestSavePath()
     {
-        string[] files = Directory.GetFiles(Application.persistentDataPath + SaveFolder, "*.sav");
+        string[] files = Directory.GetFiles(saveFolder, "*.sav");
         if (files.Length > 0)
         {
             Array.Sort(files, StringComparer.Ordinal);
@@ -178,34 +162,23 @@ public class SaveSystem : MonoBehaviour
         return null;
     }
 
-    public void ClearSave(string saveFileName = null)
-    {
-        string path = string.IsNullOrEmpty(saveFileName)
-            ? GetLatestSavePath()
-            : Application.persistentDataPath + SaveFolder + saveFileName;
-
-        if (!string.IsNullOrEmpty(path) && File.Exists(path))
-        {
-            File.Delete(path);
-            Debug.Log("Zapis " + path + " zosta³ usuniêty.");
-        }
-        else
-        {
-            Debug.LogWarning("Nie znaleziono pliku zapisu do usuniêcia.");
-        }
-
-        ClearGameData();
-    }
-
     public void ClearAllSaves()
     {
-        string[] files = Directory.GetFiles(Application.persistentDataPath + SaveFolder, "*.sav");
+        string[] files = Directory.GetFiles(saveFolder, "*.sav");
         foreach (var file in files)
         {
-            File.Delete(file);
+            try
+            {
+                File.Delete(file);
+                Debug.Log("Plik usuniêty: " + file);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Nie uda³o siê usun¹æ pliku: " + file + " - B³¹d: " + e.Message);
+            }
         }
+
         ClearGameData();
-        Debug.Log("Wszystkie zapisy zosta³y wyczyszczone.");
     }
 
     private void ClearGameData()
@@ -237,6 +210,7 @@ public class SaveSystem : MonoBehaviour
         }
         arrowGameObjects.Clear();
 
+        Resources.UnloadUnusedAssets();
         Debug.Log("Wszystkie dane gry zosta³y wyczyszczone.");
     }
 }
